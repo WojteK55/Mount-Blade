a1a51613ec260c8f25c9b3860003f93859df5a5b
Wojtek <Wojtekkk@mail.ru>
2021-10-25 20:05:54 +0300

    git init git commit commit git commit
    git init

---
+++ MountAndBlade II/Character.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MountAndBlade_II
+{
+    class Character
+    {
+
+        private int health;
+        private int damage;
+
+
+        public void Menu()
+        {
+            Console.WriteLine("***Добро пожаловать в Mount&Blade**");
+            Console.WriteLine("Сегодня вы сразитесь лицом к лицу с настоящим гладиатором");
+            Console.WriteLine("**********************************************************");
+ Console.WriteLine("Победа в сражении – это самая незначительная победа. Победа без сражения – вот высочайшее мастерство.");
+
+
+
+        }
+
+       
+
+
+
+
+        public virtual int HP()
+        {
+            return health;
+        }
+
+        public void Health_1()
+        {
+            Random rnd = new Random();
+            health = rnd.Next(90, 100);
+            Console.WriteLine("Твое здоровьей равно " + health);
+        }
+
+        public void Health_2()
+        {
+            Random rnd = new Random();
+            health = rnd.Next(90, 100);
+            Console.WriteLine("Здоровье твоего противника равно " + health);
+        }
+
+        public int Damage()
+        {
+            return damage;
+        }
+
+
+
+
+        public void Attack(Archer Archer)
+        {
+
+
+            Random rnd = new Random();
+            damage = rnd.Next(0, 35);
+            health = Archer.health - damage;
+          
+            Console.WriteLine("Твой урон составляет " + damage);
+
+            Console.WriteLine("Здоровье противника после твоего удара равно " + health);
+            
+            if (health<=0)
+            {
+                health = 0;
+            }
+
+
+        }
+        public void Attack_Back(Character Hero)
+        {
+
+            Random rnd = new Random();
+            damage = rnd.Next(0, 35);
+            health = Hero.health - damage;
+
+            Console.WriteLine("Урон противника составляет " + damage);
+            Console.WriteLine("Твоё здоровье после удара противника равно " + health);
+            
+            if (health<=0)
+            {
+                health = 0;
+            }
+
+        }
+
+    }
+
+
+
+
+
+}
---
+++ MountAndBlade II/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MountAndBlade_II
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            Character Hero = new Character();
+            Archer Archer = new Archer();
+
+            Hero.Menu();
+
+            Console.ReadKey();
+            
+            Hero.Health_1();
+            Archer.Health_2();
+            Console.ReadKey();
+
+            for (int i = 0; i < 100; i++)
+            {
+               
+
+                Hero.Attack_Back(Hero);
+
+
+                Archer.Attack(Archer);
+                Console.ReadKey();
+
+                
+
+                if (Hero.HP() <= 0)
+                {
+                    Console.WriteLine("К сожалению, Вы скоропостижнулись:(");
+                    Console.WriteLine("Важно не то, проигрываем ли мы в игре, важно, как мы проигрываем и как мы благодаря этому изменимся, что нового вынесем для себя, как сможем применить это в других играх.");
+                    Console.WriteLine("Странным образом поражение оборачивается победой.");
+                 
+                    break;
+
+                }
+                if (Archer.HP() <= 0)
+                {
+                    
+                    Console.WriteLine("Поздравляю, Ваш противник скоропостижнулся:)");
+                    Console.WriteLine("После опьянения победой возникает всегда чувство великой потери: наш враг, наш враг мертв! Даже о потере друга мы жалеем не так глубоко, как о потере врага.");
+                    break;
+                }
+
+
+            }
+
+            
+
+
+
+        }
+    }
+}
